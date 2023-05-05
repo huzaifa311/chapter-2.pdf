8614dbccef0b3cf9b541e146504455cf605485e7
huzaifa311 <119949757+huzaifa311@users.noreply.github.com>
2023-05-05 22:08:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+//                                     VARIABLES FOR STRINGS
+
+/*Question no 01: Declare a variable called username*/
+var username;
+console.log(username)
+
+/*Question no 02: Declare a variable called myName & assign to it a string that represents your Full Name*/
+var myName = prompt("Enter your Full Name");
+console.log(myName);
+
+/*Question no 03: Write script to
+a) Declare a JS variable, titled message.
+b) Assign “Hello World” to variable message
+c) Display the message in alert box.*/
+var message = "Hello World";
+alert(message);
+
+/*Question no 04: Write a script to save student’s bio data in JS variables and show the data in alert boxes.*/
+var name = prompt("Enter your Full Name");
+var age = prompt("Enter your Age");
+var experience = "Certified Mobile Application Development";
+alert("Name: " + name);
+alert(age + " years old");
+alert("Experience: " + experience);
+
+/*Question no 05: Write a script to display the following alert using one JS variable:*/
+var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+console.log(pizza);
+
+/*Question no 06: Declare a variable called email and assign to it a string that
+represents your Email Address(e.g. example@example.com).
+Show the blow mentioned message in an alert box.(Hint: use string concatenation)*/
+var email = prompt("example@example.com");
+alert("My email address is " + email);
+
+/*Question no 07: Declare a variable called book & give it the value “A
+smarter way to learn JavaScript”. Display the following message in an alert box:*/
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book '  " + book + "  '.");
+
+/*Question no 08: Write a script to display this in browser through JS*/
+document.write("<p>Yah! I can write HTML content through JavaScript</p>")
+
+/*Question no 09: */
+var string = "▬ஜ۩۞۩ஜ";
+alert(string);
+
+
